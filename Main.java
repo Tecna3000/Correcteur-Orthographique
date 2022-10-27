f07aaa517fb105f16c5b1b24b6279c3abf8af6f0
r20031646 <ramdani.meriem2001@gmail.com>
2022-10-27 15:37:05 +0200

    start qst6

--- Main.java
+++ Main.java
@@ -5,12 +5,16 @@ public class Main {
 
 
     public static void main(String[] args) throws FileNotFoundException {
-     Trigramme t = new Trigramme(new File("/amuhome/r20031646/Bureau/Licence 3/Algo2/correcteur-tp2algo2/dico.txt"));
-       //System.out.println(t);
-       //System.out.println(Trigramme.checkCorrection("abcèdert"));
+        displayTrigramme();
+       System.out.println(Trigramme.checkCorrection("abcèdert"));
       //System.out.println(Trigramme.maxCommunTrigramme("abcèdert"));
       System.out.println( Levenshtein.distance("niche","chiens"));
 
+    }
+
+    public static void displayTrigramme() throws FileNotFoundException {
+        Trigramme trigramme = new Trigramme(new File("/amuhome/r20031646/Bureau/Licence 3/Algo2/correcteur-tp2algo2/dico.txt"));
+       // System.out.println(trigramme);
     }
 
 
--- Trigramme.java
+++ Trigramme.java
@@ -65,22 +65,24 @@ public class Trigramme {
         return myString.toString();
     }
 
-    public static boolean checkCorrection(String word) {
+    //si le mot est dans le dictionnaire, il est correctement orthographié et le processus s’arrête
+    public static Map<String, Integer> checkCorrection(String word) {
         for (Map.Entry<String, List<String>> string : dictionary.entrySet()) {
-            if (string.getValue().contains(word)) return true;
+            if (string.getValue().contains(word)) break;
         }
-        System.out.println(communTrigramme(word));
-        return false;
+        return communTrigramme(word) ;//Sinon :
     }
+
     public static Map<String, Integer> communTrigramme(String word){
 
-        List<String> trigrammes = trigramme(word);
+        List<String> trigrammes = trigramme(word);//2. construire la liste des trigrammes du mot M
         Map<String,Integer> communTrigramme = new HashMap<>();
 
+        // 3. Construire la liste L des mots qui ont au moins un trigramme commun avec M
+        //4. pour chaque mot de L, calculer son nombre d’occurrences dans les listes de mots associées aux trigrammes de M
         for (Map.Entry<String, List<String>> listEntry : dictionary.entrySet()) {
             for (String string : trigrammes) {
                 if (Objects.equals(listEntry.getKey(), string)){
-                    System.out.println(listEntry.getKey());
                     for(String string2 : listEntry.getValue()){
                         if(!communTrigramme.containsKey(string2)) communTrigramme.put(string2,1);
                         else {
@@ -93,6 +95,8 @@ public class Trigramme {
         }
         return communTrigramme;
     }
+
+    //5. sélectionner les mots du dictionnaire qui ont le plus de trigrammes communs avec M
      public static Map<String, Integer> maxCommunTrigramme(String word){
          Map<String, Integer>communTrigramme = communTrigramme(word);
          int max= 1;
@@ -105,5 +109,20 @@ public class Trigramme {
 
          }
          return maxCommunTrigramme;
+     }
+
+     public void closestWords(String word){
+         Map<String, Integer> maxCommunTrigramme = maxCommunTrigramme(word);
+         Map<String, Integer> distances = new HashMap<>();
+         List<Integer> list = new ArrayList<>();
+         int max = Integer.MAX_VALUE;
+         for (Map.Entry<String, Integer> string : maxCommunTrigramme.entrySet()){
+            int distance = Levenshtein.distance(word, String.valueOf(string));
+
+            distances.put(string.getKey(),distance);
+             list.add(string.getValue());
+         }
+         Collections.sort(list);
+
      }
 }
